39fec14ef005499b7cc71d8dae9c782b23bb29c8
Marco Starker <marco.starker@gmx.net>
2021-09-03 09:32:57 +0200

    implementation

---
+++ src/index.js
@@ -0,0 +1,51 @@
+/**
+ *
+ * @param {int} duration
+ * @param args
+ * @returns {Promise<void>}
+ */
+export default function wait(duration, ...args) {
+    if (duration instanceof Function) {
+        return wait.condition(duration, ...args);
+    }
+
+    return new Promise((resolve) => setTimeout(resolve, duration));
+}
+
+/**
+ * This callback is displayed as a global member.
+ * @callback ConditionCallback
+ * @param {int} iteration
+ * @return {Promise|Boolean}
+ */
+
+/**
+ *
+ * @param {ConditionCallback} condition
+ * @param {int} duration
+ * @param {int} [maxIterations]
+ * @returns {Promise<void>}
+ */
+export const condition = wait.condition = async function condition(condition, duration, maxIterations) {
+    let result = false;
+    let i      = 0;
+
+    do {
+        await wait(duration);
+
+        result = Boolean(await condition(i));
+
+        i++;
+        if (result === false && maxIterations !== undefined && i >= maxIterations) {
+            throw new Error('Condition never succeeded in iteration interval');
+        }
+    } while (result === false);
+}
+
+/**
+ *
+ * @returns {Promise<void>}
+ */
+export const later = wait.later = function later() {
+    return wait(0);
+}
